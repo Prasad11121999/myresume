4faa279438caff6b485d1baf8173651649a8f483
Prasad Sawant <prasadsawant11121999@gmail.com>
2024-03-29 16:32:23 +0530

    Adding loader to button

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -5,7 +5,13 @@ const Button=({text,onClick,isDisabled,isLoading,className})=>{
 
     return(
         <button className={'DiPraxias-btn DiPraxias-btn-sm DiPraxias-btn-primary'+ className} onClick={onClick} disabled={isLoading?isLoading:isDisabled}>
-            {(!isLoading)?text:'Loading...'}
+            {(!isLoading)?text:
+            <>
+                <div className="spinner-border spinner-border-sm" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div><span className="ps-2">Loading...</span>
+            </>
+            }
         </button>
     );
 };
